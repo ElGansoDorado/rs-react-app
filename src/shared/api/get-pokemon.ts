84c2839aff7a4b21c86ab2968419dbcf8915fbbc
ElCarambola <yakovchikdenis@gmail.com>
2025-07-13 16:40:22 +0300

    refactor: change structure get pokemon request

--- src/shared/api/get-pokemon.ts
+++ src/shared/api/get-pokemon.ts
@@ -5,17 +5,20 @@ import type {
 } from '../model/pokemon.type';
 
 const PATH = `https://pokeapi.co/api/v2/pokemon/`;
+let cash_pokemons: PokemonType[] = [];
 
 export async function getPokemons(search: string) {
+  if (cash_pokemons.length === 0) {
+    cash_pokemons = await getPokemonPath();
+  }
+
+  return cash_pokemons.filter((item) => item.name.includes(search));
+}
+
+async function getPokemonPath() {
   const response = await fetch(PATH);
-
   const pokemonLists: PokemonResponse = await response.json();
-  const list =
-    search === ''
-      ? pokemonLists.results
-      : pokemonLists.results.filter((item) => item.name.includes(search));
-
-  return await getPokemonsType(list);
+  return getPokemonsType(pokemonLists.results);
 }
 
 async function getPokemonsType(list: Pokemon[]) {
