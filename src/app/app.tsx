1fbbcff305f22e1f9f422caed53c70dbe4242eee
ElCarambola <yakovchikdenis@gmail.com>
2025-08-08 15:22:55 +0300

    feat: add button clear cache

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,6 +4,7 @@ import ExportControls from '@/shared/ui/export-controls/export-controls';
 import { Outlet } from 'react-router-dom';
 import { useTheme } from '@/shared/hooks/use-theme';
 import { useBag } from '@/shared/hooks/use-bag';
+import { queryClient } from '@/shared/api/query-client';
 
 function App() {
   const length = useBag((state) => state.list.length);
@@ -13,9 +14,14 @@ function App() {
     <div className={`color ${theme}`}>
       <Header />
       <Outlet />
+
       <Modal show={length > 0}>
         <ExportControls />
       </Modal>
+
+      <button className="button-error" onClick={() => queryClient.clear()}>
+        clear cache
+      </button>
 
       <footer className="footer">
         <p className="footer__text">@2025 Yakovchik Denis</p>
