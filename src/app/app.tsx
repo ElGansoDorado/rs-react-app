4daf053b59b653227e391957aa1fc7d8f9c5630f
ElCarambola <yakovchikdenis@gmail.com>
2025-08-21 17:17:26 +0300

    init: pretier

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      
+      <header className="container">Header</header>
+      <main className="container">Main</main>
+      <footer className="container">@2025 Yakovchik Denis</footer>
     </>
   )
 }
